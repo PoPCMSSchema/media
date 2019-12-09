88ae69d8c9a8c792d088fb37e810f95ee856918a
Leo <losoviz@gmail.com>
2019-12-09 15:41:11 +0800

    Migrated media

--- src/Component.php
+++ src/Component.php
@@ -4,6 +4,7 @@ namespace PoP\Media;
 use PoP\Root\Component\AbstractComponent;
 use PoP\Root\Component\YAMLServicesTrait;
 use PoP\ComponentModel\Container\ContainerBuilderUtils;
+use PoP\Media\Conditional\Posts\ComponentBoot;
 
 /**
  * Initialize component
@@ -33,5 +34,10 @@ class Component extends AbstractComponent
 
         // Initialize classes
         ContainerBuilderUtils::attachFieldResolversFromNamespace(__NAMESPACE__.'\\FieldResolvers');
+
+        // Initialize all conditional clases
+        if (class_exists('\PoP\Posts\Component')) {
+            ComponentBoot::boot();
+        }
     }
 }
