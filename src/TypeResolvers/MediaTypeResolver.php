dd77635daa240a51deda7424674e89b922c22af9
Leonardo Losoviz <leo@getpop.org>
2020-09-02 13:36:47 +0800

    Added type object to $resultItem

--- src/TypeResolvers/MediaTypeResolver.php
+++ src/TypeResolvers/MediaTypeResolver.php
@@ -23,7 +23,7 @@ class MediaTypeResolver extends AbstractTypeResolver
         return $translationAPI->__('Media elements (such as images, videos, etc), attached to a post or independent', 'media');
     }
 
-    public function getID($resultItem)
+    public function getID(object $resultItem)
     {
         $cmsmediaresolver = \PoPSchema\Media\ObjectPropertyResolverFactory::getInstance();
         $media = $resultItem;
